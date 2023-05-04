f415d6f862e08577f4d725990492b7bf97277cb7
Haleigh Rzonca <hgielah@gmail.com>
2023-05-04 14:01:38 -0700

    file set up

---
+++ professional-portfolio/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Experience from './components/Experience/Experience'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import Portfolio from './components/Portfolio/Portfolio'
+import Services from './components/Services/Services'
+import Testimonials from './components/Testimonials/Testimonials'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Nav />
+    <About />
+    <Experience/>
+    <Testimonials/>
+    <Services />
+    <Portfolio/>
+    <Contact />
+    <Footer/>
+    </>
+
+  )
+}
+
+export default App
